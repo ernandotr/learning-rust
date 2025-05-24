0438a3f015659a784216d40e8ee405e6db94e42b
Ernando Rezende <ernando.03@gmail.com>
2025-05-24 23:28:51 +0100

    Demonstrating the use of a constant

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -9,6 +9,9 @@ fn main() {
 
     let number1 = 7;
     let number2 = 20;
+
+    // Performing various arithmetic operations
+    println!("\nArithmetic operations between {} and {}:", number1, number2);
     let sum = number1 + number2;
     println!("The sum of {} and {} is {}.", number1, number2, sum);
     let product = number1 * number2;
@@ -19,43 +22,8 @@ fn main() {
     println!("The quotient of {} and {} is {}.", number1, number2, quotient);
     let remainder = number1 % number2;
     println!("The remainder of {} divided by {} is {}.", number1, number2, remainder);
-    let is_greater = number1 > number2;
-    println!("Is {} greater than {}? {}", number1, number2, is_greater);
-    let is_less = number1 < number2;
-    println!("Is {} less than {}? {}", number1, number2, is_less);
-    let is_equal = number1 == number2;
-    println!("Is {} equal to {}? {}", number1, number2, is_equal);
-    let is_not_equal = number1 != number2;
-    println!("Is {} not equal to {}? {}", number1, number2, is_not_equal);
-    let is_greater_or_equal = number1 >= number2;
-    println!("Is {} greater than or equal to {}? {}", number1, number2, is_greater_or_equal);
-    let is_less_or_equal = number1 <= number2;
-    println!("Is {} less than or equal to {}? {}", number1, number2, is_less_or_equal);
-    let is_even = number1 % 2 == 0;
-    println!("Is {} even? {}", number1, is_even);
-    let is_odd = number1 % 2 != 0;
-    println!("Is {} odd? {}", number1, is_odd);
-    let is_positive = number1 > 0;
-    println!("Is {} positive? {}", number1, is_positive);
-    let is_negative = number1 < 0;
-    println!("Is {} negative? {}", number1, is_negative);
-    let is_zero = number1 == 0;
-    println!("Is {} zero? {}", number1, is_zero);
-    let is_non_zero = number1 != 0;
-    println!("Is {} non-zero? {}", number1, is_non_zero);
-    let is_prime = is_prime(number1);
-    println!("Is {} prime? {}", number1, is_prime); 
-}
-
-// Function to check if a number is prime
-fn is_prime(n: i32) -> bool {
-    if n <= 1 {
-        return false;
-    }
-    for i in 2..=((n as f64).sqrt() as i32) {
-        if n % i == 0 {
-            return false;
-        }
-    }
-    true
+    
+    // Demonstrating the use of a constant
+    const PI: f64 = 3.14159;
+    println!("\nThe value of PI is approximately {:.5}.", PI);
 }
