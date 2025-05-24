84ff5f6ea4fd1547427216996e34f522b6c9b4ac
Ernando Rezende <ernando.03@gmail.com>
2025-05-24 23:01:45 +0100

    Hello app in rust

---
+++ hello-rust/src/main.rs
@@ -0,0 +1,9 @@
+// This is a simple Rust program that prints a greeting message with a name and age.
+// You can change the values of `name` and `age` to see different outputs.
+// To run this program, use the command: cargo run
+
+fn main() {
+    let name: &'static str = "Alice"; // or let name = "Bob";
+    let age: i16 = 25; // or let age = 30;
+    println!("Hello, {} your age is {} !", name, age);
+}
