1bf0b2a4237c322f6eacb681331e62176d22060b
Ernando Rezende <ernando.03@gmail.com>
2025-05-26 00:14:23 +0100

    Created a App to sum the digits of a number

---
+++ sum-digits/src/main.rs
@@ -0,0 +1,23 @@
+use std::io;
+
+fn read_int() -> i32 {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let num = input.trim().parse::<i32>().expect("Please enter a valid integer");
+    num
+}
+
+fn main() {
+    println!("Enter an integer:");
+    let mut number = read_int();
+    let mut sum = 0;
+
+    while number !=  0 {
+        let rest  = number % 10;
+        sum += rest;
+        number = number / 10;
+    }
+
+    print!("Sum of digits: {}", sum);
+
+}
